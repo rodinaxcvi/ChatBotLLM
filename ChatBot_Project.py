4518a3be859b45cdba413028896aa6eeb06730af
rodinaxcvi <villanuevac89@yahoo.com>
2023-08-25 11:59:03 -0700

    second commit

--- ChatBot_Project.py
+++ ChatBot_Project.py
@@ -2,6 +2,10 @@ import nltk
 import ssl
 
 
+## THIS CODE DOWNLOADS WIKIPEDIA PAGES OF DIFFERENT CITIES, AND STORES THEM IN MILVUR TO BE ANALYZED
+# BY CHATGPT. ALSO MAKES A CSV OF RESPONSE AND QUERY DATA TO BE USED IN VISUALIZATION TOOLS
+
+
 '''
 The Try-Except-Else statement below is in the original code, and it disables SSL certificate verification
 when making HTTPS requests (specifically when downloading from Wikipedia).
@@ -15,10 +19,7 @@ attacks. You may need to bypass SSL verification for any reason, but that is you
 ''' 
 
 
-
-
 # Try - Except - Else statement. It is left commented out for security reasons but you may need to utilize it.
-
 '''
 try:
     _create_unverified_https_context = ssl._create_unverified_context
@@ -30,12 +31,7 @@ else:
     
 
 
-
-
-
 nltk.download("stopwords")
-
-
 from llama_index import (
     GPTVectorStoreIndex,
     GPTSimpleKeywordTableIndex,
@@ -46,12 +42,14 @@ from llama_index import (
 )
 
 
+
 # Import Langchain, which helps develop apps powered by language models
 ## Link: https://python.langchain.com/docs/get_started/introduction.html
 from langchain.chat_models import ChatOpenAI
 import os
 from dotenv import load_dotenv
 import openai
+
 
 
 
@@ -65,6 +63,9 @@ openai.api_key = openai_api_key
 
 
 # Import Milvus, which lets you store, index, and manage embedding vectors 
+import pandas as pd 
+from llama_index.storage import Embedding
+from llama_index.embeddings import EmbeddingVectorsFromText
 from llama_index.vector_stores import MilvusVectorStore
 from milvus import default_server
 
@@ -76,10 +77,17 @@ vector_store = MilvusVectorStore(
 
 
 
+# Set the LLM (Large Language Model) as well as LLM service and storage context.
+chatgpt_llm = LLMPredictor(llm=ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo"))
+service_context = ServiceContext.from_defaults(llm_predictor=chatgpt_llm)
+storage_context = StorageContext.from_defaults(vector_store=vector_store)
+vector_transformer = EmbeddingVectorsFromText(llm_predictor=chatgpt_llm, vector_store=vector_store)
+vectors_data = []
+
+
 
 # Set the titles of Wikipedia pages that you want to scrape
 wiki_titles = ["Toronto", "Seattle", "San Francisco", "Chicago", "Boston", "Washington D.C", "Cambridge, Massachusetts", "Houston"]
-
 
 from pathlib import Path
 import requests
@@ -103,6 +111,20 @@ for title in wiki_titles:
 
     with open(data_path / f"{title}.txt", 'w', encoding='utf-8') as fp:
         fp.write(wiki_text)
+    embedding = vector_transformer.transform(wiki_text)
+    vector_store.add_embedding(Embedding(embedding=embedding, metadata={'wiki_title': title}))
+
+    vectors_data.append({'wiki_title': title, 'embedding': embedding})
+
+# Convert vectors_data to a DataFrame
+vectors_df = pd.DataFrame(vectors_data)
+# Save the DataFrame to a CSV file
+vectors_df.to_csv("embedded_vectors.csv", index=False)
+
+
+
+
+
 
 
 # Load all wiki documents
@@ -110,27 +132,22 @@ city_docs = {}
 for wiki_title in wiki_titles:
     city_docs[wiki_title] = SimpleDirectoryReader(input_files=[f"data/{wiki_title}.txt"]).load_data()
     
-
-
-
-# Set the LLM (Large Language Model) as well as LLM service and storage context.
-chatgpt_llm = LLMPredictor(llm=ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo"))
-service_context = ServiceContext.from_defaults(llm_predictor=chatgpt_llm)
-storage_context = StorageContext.from_defaults(vector_store=vector_store)
-
-
-
-
 # Build city document index
 city_indices = {}
 index_summaries = {}
+
+# Add index data to a list that can be used for debugging and visualization tools like Arize-Phoenix
+visualization_data = []
+
 for wiki_title in wiki_titles:
     city_indices[wiki_title] = GPTVectorStoreIndex.from_documents(city_docs[wiki_title], service_context=service_context, storage_context=storage_context)
     # Set summary text for city
     index_summaries[wiki_title] = f"Wikipedia articles about {wiki_title}"
-
-
-
+    index_data = {
+        'wiki_title': wiki_title,
+        'index_summaries': index_summaries
+    }
+    visualization_data.append(index_data)
 
 
 from llama_index.indices.composability import ComposableGraph
@@ -141,13 +158,10 @@ graph = ComposableGraph.from_indices(
     max_keywords_per_chunk=50
 )
 
-
 from llama_index.indices.query.query_transform.base import DecomposeQueryTransform
 decompose_transform = DecomposeQueryTransform(
     chatgpt_llm, verbose=True
 )
-
-
 
 from llama_index.query_engine.transform_query_engine import TransformQueryEngine
 custom_query_engines = {}
@@ -167,13 +181,11 @@ custom_query_engines[graph.root_index.index_id] = graph.root_index.as_query_engi
 query_engine_decompose = graph.as_query_engine(
     custom_query_engines=custom_query_engines,)
 
-
 response_chatgpt = query_engine_decompose.query(
     "Compare and contrast the airports in Seattle, Houston, Toronto. "
 )
 
 print(str(response_chatgpt))
-
 
 custom_query_engines = {}
 for index in city_indices.values():
@@ -191,12 +203,22 @@ query_engine = graph.as_query_engine(
 )
 
 
-## Response from ChatGPT
 
+
+
+
+## Response from ChatGPT
 response_chatgpt = query_engine.query(
     "Compare and contrast the airports in Seattle, Houston, and Toronto. "
 
 )
-
 str(response_chatgpt)
 
+# Gather response data for visualization
+response_data = {
+    'query': "Compare and contrast the airports in Seattle, Houston, Toronto.",
+    'response': str(response_chatgpt)
+}
+visualization_data.append(response_data)
+visualization_df = pd.DataFrame(visualization_data)
+visualization_df.to_csv('visualization_data.csv', index=False)
---
+++ chatbot_analysis.py
@@ -0,0 +1,1 @@
+import ChatBot_project.py
